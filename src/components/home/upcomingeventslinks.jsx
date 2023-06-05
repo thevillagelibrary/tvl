ac3c57e10b107d7b96728a17adac57674474a0db
jeffreystorer <jeffrey.storer@gmail.com>
2023-06-05 11:14:16 -0400

    6/5: Change Upcoming Events Format

--- src/components/home/upcomingeventslinks.jsx
+++ src/components/home/upcomingeventslinks.jsx
@@ -4,28 +4,14 @@ import { v4 as uuidv4 } from 'uuid';
 export default function UpcomingEventsLinks({ futureEvents }) {
   const futureEventCount = futureEvents.length;
 
-  function PhoneEvents() {
+  function Events() {
     return (
-      <table id='tbl_upcoming-events' className='visibility_not-phone'>
-        <tbody>
-          {futureEvents.map((event, index) => (
-            <tr key={uuidv4()} className='tr_event'>
-              <td>{event[3]}</td>
-              <td>{event[5]}</td>
-              <td>{event[2]}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
-
-  function NotPhoneEvents() {
-    return (
-      <dl key={uuidv4()} id='dl_upcoming-events' className='visibility_phone'>
+      <dl key={uuidv4()} id='dl_upcoming-events'>
         {futureEvents.map((event, index) => (
           <div key={uuidv4()}>
-            <dt>{event[3]}</dt>
+            <dt>
+              <h3>{event[3]}</h3>
+            </dt>
             <dd>
               {event[5]}, {event[2]}
             </dd>
@@ -43,8 +29,7 @@ export default function UpcomingEventsLinks({ futureEvents }) {
         <Link id='a_event-link' to='/newsevents/upcomingevents'>
           Click here for details.
         </Link>
-        <NotPhoneEvents />
-        <PhoneEvents />
+        <Events />
       </>
     );
   }
