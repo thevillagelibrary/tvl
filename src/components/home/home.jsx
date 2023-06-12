d098ab2f410a81c97a4dfa179b0b7a8dd7d60b72
jeffreystorer <jeffrey.storer@gmail.com>
2023-06-12 10:47:26 -0400

    6/12: Fix Divider on Home Page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -43,7 +43,7 @@ export default function Home() {
             </article>
           </section>
           <br />
-          <hr className='hr_divider' />
+          <hr className='divider' />
           <article>
             <UpcomingEventsLinks futureEvents={futureEvents} />
           </article>
