c0523d8a0b655513c3ad01e4d13925254fe608bc
jeffreystorer <jeffrey.storer@gmail.com>
2024-02-06 20:03:38 -0500

    2/6: corrrect TVL email address

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -42,7 +42,7 @@ export default function Footer() {
         </Link>
       </section>
       <section>
-        <Link to='mailto:thevillagelibrary.inc@gmail.com'>
+        <Link to='mailto:thevillagelibrary.org@gmail.com'>
           Contact our Webmaster
         </Link>
       </section>
