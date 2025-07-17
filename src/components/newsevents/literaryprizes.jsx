2199ecbca636843f8375a4ef3d2309d18729295e
jeffreystorer <jeffrey.storer@gmail.com>
2025-07-17 09:07:51 -0400

    7/17: update prizes

--- src/components/newsevents/literaryprizes.jsx
+++ src/components/newsevents/literaryprizes.jsx
@@ -6,7 +6,7 @@ export default function LiteraryPrizes() {
     <PageBodySideMenu name={'LiteraryPrizes'}>
       <section id='prizes'>
         <article>
-          <h2>The Pulitzer Prizes:</h2>
+          <h2>Pulitzer Prizes:</h2>
           <a href='https://www.pulitzer.org/prize-winners-by-category/219'>
             Fiction
           </a>
@@ -35,13 +35,16 @@ export default function LiteraryPrizes() {
             General Nonfiction
           </a>
           <div className='divider'></div>
-          <h2>The Booker Prizes:</h2>
+          <h2>Booker Prize:</h2>
           <a href='https://thebookerprizes.com/the-booker-library/features/full-list-of-booker-prize-winners-shortlisted-authors-and-their-books'>
             Domestic Fiction
           </a>
           <a href='https://thebookerprizes.com/the-booker-library/features/full-list-of-international-booker-prize-winners-shortlisted-authors-and-their-books'>
             International Fiction
           </a>
+          <div className='divider'></div>
+          <h2>Theakston Awards:</h2>
+          <a href='https://www.harrogatetheakstoncrimeaward.com/'>Old Peculier Crime Novel</a>
         </article>
         <article>
           <h2>National Book Awards:</h2>
@@ -56,12 +59,15 @@ export default function LiteraryPrizes() {
           <h2>Barry Awards:</h2>
           <a href='http://new.deadlypleasures.com/barry-awards/'>Mysteries and Crime Novels</a>
           <div className='divider'></div>
-          <h2>Gold Dagger Awards:</h2>
-          <a href='https://thecwa.co.uk/awards-and-competitions/the-daggers/gold-dagger/'>Best Crime Novel</a>
+          <h2>Crime Writers Association:</h2>
+          <a href='https://thecwa.co.uk/awards-and-competitions/the-daggers/'>Dagger Awards</a>
           <div className='divider'></div>
           <h2>Macavity Awards:</h2>
           <a href='https://mysteryreaders.org/macavity-awards/'>Mysteries</a>
-        </article>
+          <div className='divider'></div>
+          <h2>Australian Crime Writers Association</h2>
+          <a href='https://www.austcrimewriters.com/the-ned-kelly-awards'>The Ned Kelly Awards</a>
+          </article>
       </section>
     </PageBodySideMenu>
   );
